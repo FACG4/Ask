d2366a45b85eaf494e4c43f33ea5691efb4a83d2
bnazmi <6nazmi@gmail.com>
2018-04-18 16:01:28 +0300

    add questions

--- src/queries/getData.js
+++ src/queries/getData.js
@@ -1,7 +1,7 @@
 const dbConnections= require('./../database/db_connection');
 
 const getUserData=(user_id,cb)=>{
-    dbConnections.query(`select questions.body as questionBody ,answers.body  as answerBody from questions inner join answers on questions.id=answers.question_id where questions.user_id=${user_id}`,(err,res)=>{
+    dbConnections.query(`select questions.body as questionBody ,answers.body  as answerBody from questions left join answers on questions.id=answers.question_id where questions.user_id=${user_id}`,(err,res)=>{
         if(err) cb(err);
         else{
             cb(null,res.rows);
