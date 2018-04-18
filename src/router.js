18926c90fe67fbc6b6cb4891e6772afe8de2fd19
Balsam-Faysal <blsam.2332016@gmail.com>
2018-04-18 16:05:46 +0300

    solve conflicts

--- src/router.js
+++ src/router.js
@@ -1,112 +1,133 @@
-const fs = require('fs');
-const path = require('path');
-const getData = require('./queries/getData');
+const fs = require("fs");
+const path = require("path");
+const postData = require("./queries/postData");
+const getData = require("./queries/getData");
+const queryString = require("querystring");
 const router = (req, response) => {
-
   const endpoint = req.url;
-  if (endpoint === "/") {
-    fs.readFile(path.join(__dirname, '..', 'public', 'html', 'home_page.html'), (err, file) => {
-      if (err) {
-        throw new Error(err);
-      } else {
-        response.writeHead(200, {
-          'Content-Type': 'text/html'
-        });
-        response.end(file);
-      }
-    });
-  }
-  else if (endpoint.split('/')[1]==="user") {
-    fs.readFile(path.join(__dirname, '..', 'public', 'html', 'user_page.html'), (err, file) => {
-      if (err) {
-        throw new Error(err);
-      } else {
-        response.writeHead(200, {
-          'Content-Type': 'text/html'
-        });
-        response.end(file);
-      }
-    });
-  }
-  else if (endpoint === "/css/style.css") {
-    fs.readFile(path.join(__dirname, '..', 'public', 'css', 'style.css'), (err, file) => {
-      if (err) {
-        throw new Error(err);
-      } else {
-        response.writeHead(200, {
-          'Content-Type': 'text/css'
-        });
-        response.end(file);
-      }
-    });
 
+  if (endpoint === "/") {
+    fs.readFile(
+      path.join(__dirname, "..", "public", "html", "home_page.html"),
+      (err, file) => {
+        if (err) {
+          throw new Error(err);
+        } else {
+          response.writeHead(200, {
+            "Content-Type": "text/html"
+          });
+          response.end(file);
+        }
+      }
+    );
+  } else if (endpoint.split("/")[1] === "user") {
+    fs.readFile(
+      path.join(__dirname, "..", "public", "html", "user_page.html"),
+      (err, file) => {
+        if (err) {
+          throw new Error(err);
+        } else {
+          response.writeHead(200, {
+            "Content-Type": "text/html"
+          });
+          response.end(file);
+        }
+      }
+    );
+  } else if (endpoint === "/css/style.css") {
+    fs.readFile(
+      path.join(__dirname, "..", "public", "css", "style.css"),
+      (err, file) => {
+        if (err) {
+          throw new Error(err);
+        } else {
+          response.writeHead(200, {
+            "Content-Type": "text/css"
+          });
+          response.end(file);
+        }
+      }
+    );
   } else if (endpoint === "/js/dom_home_page.js") {
-    fs.readFile(path.join(__dirname, '..', 'public', 'js', 'dom_home_page.js'), (err, file) => {
-      if (err) {
-        throw new Error(err);
-      } else {
-        response.writeHead(200, {
-          'Content-Type': 'text/javascript'
-        });
-        response.end(file);
+    fs.readFile(
+      path.join(__dirname, "..", "public", "js", "dom_home_page.js"),
+      (err, file) => {
+        if (err) {
+          throw new Error(err);
+        } else {
+          response.writeHead(200, {
+            "Content-Type": "text/javascript"
+          });
+          response.end(file);
+        }
       }
-    });
-  }
-  else if (endpoint === "/js/dom_user_page.js") {
-    fs.readFile(path.join(__dirname, '..', 'public', 'js', 'dom_user_page.js'), (err, file) => {
-      if (err) {
-        throw new Error(err);
-      } else {
-        response.writeHead(200, {
-          'Content-Type': 'text/javascript'
-        });
-        response.end(file);
+    );
+  } else if (endpoint === "/js/dom_user_page.js") {
+    fs.readFile(
+      path.join(__dirname, "..", "public", "js", "dom_user_page.js"),
+      (err, file) => {
+        if (err) {
+          throw new Error(err);
+        } else {
+          response.writeHead(200, {
+            "Content-Type": "text/javascript"
+          });
+          response.end(file);
+        }
       }
-    });
-  }
-   else if (endpoint === "/select") {
+    );
+  } else if (endpoint === "/select") {
     getData.getData((err, res) => {
       if (err) {
-        response.writeHead(500, 'Content-Type:text/html');
-        response.end('<h1>Sorry, there was a problem getting the users</h1>');
+        response.writeHead(500, "Content-Type:text/html");
+        response.end("<h1>Sorry, there was a problem getting the users</h1>");
         console.log(error);
       } else {
         let output = JSON.stringify(res);
         response.writeHead(200, {
-          'content-type': 'application/json'
+          "content-type": "application/json"
         });
         response.end(output);
       }
     });
-
-  }else if(endpoint.split('/')[1]==="selectUser"){
-    let id =endpoint.split('/')[2];
-    getData.getUserData((err,res)=>{
-        if (err) {
-            response.writeHead(500, 'Content-Type:text/html');
-            response.end('<h1>Sorry, there was a problem getting the users</h1>');
-            console.log(err);
-          } else {
-            let output = JSON.stringify(res);
-            response.writeHead(200, {
-              'content-type': 'application/json'
-            });
-            response.end(output);
-          }
-    },id);
-}else{
-  fs.readFile(path.join(__dirname, '..', 'public', 'html', 'error.html'), (err, file) => {
-    if (err) {
-      throw new Error(err);
-    } else {
-      response.writeHead(200, {
-        'Content-Type': 'text/html'
+  } else if (endpoint.split("/")[1] === "selectUser") {
+    console.log("res");
+    let id = endpoint.split("/")[2];
+    getData.getUserData(id, (err, res) => {
+      if (err) {
+        response.writeHead(500, "Content-Type:text/html");
+        response.end("<h1>Sorry, there was a problem getting the users</h1>");
+        console.log(err);
+      } else {
+        let output = JSON.stringify(res);
+        response.writeHead(200, {
+          "content-type": "application/json"
+        });
+        response.end(output);
+      }
+    });
+  } else if (endpoint === "/ask-question") {
+    let body = "";
+    req.on("data", data => {
+      body += data;
+    });
+    req.on("end", () => {
+      let data = queryString.parse(body);
+      postData.askQuestion(data, (err, res) => {
+        // console.log(res);
       });
-      response.end(file);
-    }
-  });
-}
-
-}
+    });
+  } else if (endpoint === "/reply") {
+    let body = "";
+    req.on("data", data => {
+      body += data;
+    });
+    req.on("end", () => {
+      let data = queryString.parse(body);
+      postData.reply(data, (err, res) => {
+      });
+    });
+  }
+};
 
 module.exports = router;
