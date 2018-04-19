bde389347299a4140c4ab2f9cc6819279119952c
Balsam-Faysal <blsam.2332016@gmail.com>
2018-04-19 12:03:19 +0300

    resolve the issues

--- src/queries/getData.js
+++ src/queries/getData.js
@@ -1,8 +1,12 @@
 const dbConnections = require("./../database/db_connection");
 
 const getUserData = (user_id, cb) => {
+  let sql = {
+    text: "SELECT questions.id AS questionId, questions.body AS questionBody ,answers.body  AS answerBody, questions.user_id as user_id FROM questions LEFT JOIN answers ON questions.id=answers.question_id WHERE questions.user_id = $1 ORDER BY questions.id DESC",
+    values: [user_id]
+  };
   dbConnections.query(
-    `select questions.id as questionid, questions.body as questionBody ,answers.body  as answerBody, questions.user_id as user_id from questions left join answers on questions.id=answers.question_id where questions.user_id=${user_id} ORDER BY questions.id DESC;`,
+    sql,
     (err, res) => {
       if (err) cb(err);
       else {
@@ -13,7 +17,6 @@ const getUserData = (user_id, cb) => {
 };
 
 const getData = cb => {
-  
   dbConnections.query(
     "SELECT users.id , users.name , questions.body AS question, answers.body AS answer FROM users, questions , answers WHERE users.id = questions.user_id AND questions.id = answers.question_id  ORDER BY users.id ASC;",
     (err, res) => {
@@ -26,4 +29,7 @@ const getData = cb => {
   );
 };
 
-module.exports = { getUserData, getData };
+module.exports = {
+  getUserData,
+  getData
+};
